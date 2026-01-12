d38975aad6f2fb5592586c3ad99450c1e729ec69
alessandraciccone <alessandraciccone@icloud.com>
2026-01-12 14:50:40 +0100

    Initial commit

---
+++ climatechange10/src/main/java/aciccone/climatechange10/Climatechange10Application.java
@@ -0,0 +1,13 @@
+package aciccone.climatechange10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Climatechange10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Climatechange10Application.class, args);
+	}
+
+}
---
+++ climatechange10/src/test/java/aciccone/climatechange10/Climatechange10ApplicationTests.java
@@ -0,0 +1,13 @@
+package aciccone.climatechange10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Climatechange10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
